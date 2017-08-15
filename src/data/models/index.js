d2af30ddf5cec76781bb2e21acd02b96de52e8b5
Tanguy Invernizzi <tanguy.invernizzi@yahoo.fr>
2017-08-15 04:21:49 +0200

    Fixed export of models

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -12,6 +12,8 @@ import User from './User';
 import UserLogin from './UserLogin';
 import UserClaim from './UserClaim';
 import UserProfile from './UserProfile';
+import Subject from './Subject';
+import Argument from './Argument';
 
 User.hasMany(UserLogin, {
   foreignKey: 'userId',
@@ -39,4 +41,4 @@ function sync(...args) {
 }
 
 export default { sync };
-export { User, UserLogin, UserClaim, UserProfile };
+export { User, UserLogin, UserClaim, UserProfile, Subject, Argument };
